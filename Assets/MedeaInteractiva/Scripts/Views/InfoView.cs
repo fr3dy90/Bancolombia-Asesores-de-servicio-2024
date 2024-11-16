b1340cef4bbf9c6f94dadc2e78197c5d442a4d36
Fr3dz <john.espinosa@polygon.us>
2024-11-16 10:24:14 -0500

    project optimized

--- Assets/MedeaInteractiva/Scripts/Views/InfoView.cs
+++ Assets/MedeaInteractiva/Scripts/Views/InfoView.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 public class InfoView : BaseView
 {
     [SerializeField] private TextMeshProUGUI _textToDisplay;
+    [SerializeField] private TextMeshProUGUI _shadowtextToDisplay;
 
     public void SetInfoText(string str)
     {
         _textToDisplay.text = str;
+        _shadowtextToDisplay.text = str;
     }
 }
